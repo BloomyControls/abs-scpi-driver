32f7c2bec0499186b5133190d11f74cf8cacf05a
Will Eccles <william.r.eccles@bloomy.com>
2024-05-10 12:05:34 -0400

    initial project setup

---
+++ include/bci/abs/ScpiDriver.h
@@ -0,0 +1,15 @@
+#ifndef ABS_SCPI_DRIVER_INCLUDE_BCI_ABS_SCPIDRIVER_H
+#define ABS_SCPI_DRIVER_INCLUDE_BCI_ABS_SCPIDRIVER_H
+
+namespace bci::abs {
+
+class ScpiDriver {
+ public:
+  ScpiDriver();
+
+  ~ScpiDriver();
+};
+
+}  // namespace bci::abs
+
+#endif  /* ABS_SCPI_DRIVER_INCLUDE_BCI_ABS_SCPIDRIVER_H */
---
+++ src/ScpiDriver.cpp
@@ -0,0 +1,9 @@
+#include <bci/abs/ScpiDriver.h>
+
+namespace bci::abs {
+
+ScpiDriver::ScpiDriver() {}
+
+ScpiDriver::~ScpiDriver() {}
+
+}  // namespace bci::abs
