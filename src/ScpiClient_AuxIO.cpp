9d93742db65ae650845c10cd18947355c7c95f1b
Will Eccles <william.r.eccles@bloomy.com>
2024-05-20 17:05:42 -0400

    license: apply header to source code

--- src/ScpiClient_AuxIO.cpp
+++ src/ScpiClient_AuxIO.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, Bloomy Controls, Inc. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-3-clause license that can be
+ * found in the LICENSE file or at https://opensource.org/license/BSD-3-Clause
+ */
+
 #include <bci/abs/ScpiClient.h>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
